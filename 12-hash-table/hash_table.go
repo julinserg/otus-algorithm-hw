5e1268557cc5e28f99ec357ed97cccc8e92b9914
ss.yulin <ss.yulin@gitlab.com>
2023-03-23 17:44:10 +0300

    hash table is done

--- 12-hash-table/hash_table.go
+++ 12-hash-table/hash_table.go
@@ -7,6 +7,7 @@ type ItemType interface {
 }
 
 const defaultBucketsCount = 11
+const defaultBucketsFill = 0.75
 
 type KeyElement[K ItemType] interface {
 	key() K
@@ -26,8 +27,10 @@ type HashTable[K, V ItemType] struct {
 }
 
 func (s *HashTable[K, V]) init() {
-	s.buckets = make([]*Element[K, V], defaultBucketsCount)
-	s.capacity = defaultBucketsCount
+	if s.capacity == 0 {
+		s.capacity = defaultBucketsCount
+	}
+	s.buckets = make([]*Element[K, V], s.capacity)
 }
 
 func (s *HashTable[K, V]) Size() int {
@@ -38,31 +41,76 @@ func (s *HashTable[K, V]) IsEmpty() bool {
 	return s.Size() == 0
 }
 
+func (s *HashTable[K, V]) rehash(capacity int) {
+	s.capacity = capacity
+	s.size = 0
+	bucketsOld := s.buckets
+
+	for i := 0; i < len(bucketsOld); i++ {
+		item := bucketsOld[i]
+		for item != nil {
+			s.Insert(item.key, item.value)
+			item = item.next
+		}
+	}
+}
+
 func (s *HashTable[K, V]) Insert(key KeyElement[K], value V) {
 	if s.IsEmpty() {
 		s.init()
 	}
+	if float64(s.size) >= float64(s.capacity)*defaultBucketsFill {
+		s.rehash(s.capacity * 2)
+	}
 	indexBucket := key.hash() % uint32(s.capacity)
 	if s.buckets[indexBucket] == nil {
 		s.buckets[indexBucket] = &Element[K, V]{key, value, nil}
+		s.size++
 	} else {
 		item := s.buckets[indexBucket]
 		for item != nil {
+			if item.key == key {
+				item.value = value
+				return
+			}
 			if item.next == nil {
 				item.next = &Element[K, V]{key, value, nil}
+				s.size++
+				return
 			}
 			item = item.next
 		}
 	}
-	s.size++
-
 }
 
-func (s *HashTable[K, V]) Remove(key KeyElement[K]) {
+func (s *HashTable[K, V]) Remove(key KeyElement[K]) bool {
 	if s.IsEmpty() {
-		return
+		return false
 	}
-	s.size--
+	if s.size*4 < s.capacity {
+		s.rehash(s.size * 2)
+	}
+	indexBucket := key.hash() % uint32(s.capacity)
+	if s.buckets[indexBucket] == nil {
+		return false
+	} else {
+		item := s.buckets[indexBucket]
+		prevItem := item
+		for item != nil {
+			if item.key == key {
+				if item == prevItem {
+					s.buckets[indexBucket] = item.next
+				} else {
+					prevItem.next = item.next
+				}
+				s.size--
+				return true
+			}
+			prevItem = item
+			item = item.next
+		}
+	}
+	return false
 }
 
 func (s *HashTable[K, V]) Search(key KeyElement[K]) V {
@@ -70,8 +118,19 @@ func (s *HashTable[K, V]) Search(key KeyElement[K]) V {
 	if s.IsEmpty() {
 		return value
 	}
+	indexBucket := key.hash() % uint32(s.capacity)
+	if s.buckets[indexBucket] == nil {
+		return value
+	} else {
+		item := s.buckets[indexBucket]
+		for item != nil {
+			if item.key == key {
+				return item.value
+			}
+			item = item.next
+		}
+	}
 	return value
-
 }
 
 func (s *HashTable[K, V]) Print() {
@@ -84,6 +143,14 @@ func (s *HashTable[K, V]) Print() {
 	}
 }
 
-func (s *HashTable[K, V]) ListKey() []int {
-	return []int{}
+func (s *HashTable[K, V]) ListKey() []KeyElement[K] {
+	result := make([]KeyElement[K], 0)
+	for i := 0; i < s.capacity; i++ {
+		item := s.buckets[i]
+		for item != nil {
+			result = append(result, item.key)
+			item = item.next
+		}
+	}
+	return result
 }
--- 12-hash-table/hash_table_test.go
+++ 12-hash-table/hash_table_test.go
@@ -2,7 +2,11 @@ package p12hashtable
 
 import (
 	"hash/fnv"
+	"math/rand"
+	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type KeyElementString struct {
@@ -19,9 +23,69 @@ func (s KeyElementString) hash() uint32 {
 	return h.Sum32()
 }
 
+func testEq(a []KeyElement[string], b []KeyElementString) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i].key() != b[i].key() {
+			return false
+		}
+	}
+	return true
+}
+
 func TestHashTable(t *testing.T) {
 	hashTable := &HashTable[string, string]{}
 	hashTable.Insert(KeyElementString{"mykey1"}, "mavalue1")
 	hashTable.Insert(KeyElementString{"mykey2"}, "mavalue2")
 	hashTable.Print()
+
+	require.Equal(t, true, testEq(hashTable.ListKey(), []KeyElementString{{"mykey2"}, {"mykey1"}}))
+
+	require.Equal(t, "mavalue2", hashTable.Search(KeyElementString{"mykey2"}))
+	require.Equal(t, "mavalue1", hashTable.Search(KeyElementString{"mykey1"}))
+	require.Equal(t, "", hashTable.Search(KeyElementString{"mykey0"}))
+
+	require.Equal(t, 2, hashTable.Size())
+	require.Equal(t, true, hashTable.Remove(KeyElementString{"mykey1"}))
+	require.Equal(t, true, hashTable.Remove(KeyElementString{"mykey2"}))
+	require.Equal(t, false, hashTable.Remove(KeyElementString{"mykey0"}))
+	require.Equal(t, 0, hashTable.Size())
+
+	hashTable.Insert(KeyElementString{"mykey1"}, "mavalue1")
+	hashTable.Insert(KeyElementString{"mykey2"}, "mavalue2")
+	require.Equal(t, true, testEq(hashTable.ListKey(), []KeyElementString{{"mykey2"}, {"mykey1"}}))
+}
+
+func TestHashTableWithRehash(t *testing.T) {
+	hashTable := &HashTable[string, string]{}
+	N := 100000
+	for i := 0; i < N; i++ {
+		r := rand.Intn(N)
+		rStr := strconv.Itoa(r)
+		hashTable.Insert(KeyElementString{rStr}, rStr)
+		hashTable.Insert(KeyElementString{strconv.Itoa(i)}, strconv.Itoa(i))
+		hashTable.Insert(KeyElementString{rStr}, rStr)
+	}
+	require.Equal(t, N, hashTable.Size())
+
+	hashTable.Insert(KeyElementString{"mykey1"}, "mavalue1")
+	hashTable.Insert(KeyElementString{"mykey2"}, "mavalue2")
+	require.Equal(t, "mavalue2", hashTable.Search(KeyElementString{"mykey2"}))
+	require.Equal(t, "mavalue1", hashTable.Search(KeyElementString{"mykey1"}))
+	require.Equal(t, "", hashTable.Search(KeyElementString{"mykey0"}))
+	require.Equal(t, true, hashTable.Remove(KeyElementString{"mykey1"}))
+	require.Equal(t, true, hashTable.Remove(KeyElementString{"mykey2"}))
+
+	require.Equal(t, N, hashTable.Size())
+
+	for i := 0; i < N; i++ {
+		require.NotEqual(t, "", hashTable.Search(KeyElementString{strconv.Itoa(i)}))
+	}
+
+	for i := 0; i < N; i++ {
+		require.Equal(t, true, hashTable.Remove(KeyElementString{strconv.Itoa(i)}))
+	}
+	require.Equal(t, 0, hashTable.Size())
 }
