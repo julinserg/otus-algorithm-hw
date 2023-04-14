b04fc815625e983616405df67869fd286a63525c
ss.yulin <ss.yulin@gitlab.com>
2023-04-14 18:51:38 +0300

    dont work

--- 14-graph-strongly-connected-component/graph_strongly_connected_component.go
+++ 14-graph-strongly-connected-component/graph_strongly_connected_component.go
@@ -1,12 +1,18 @@
 package p14graphstronglyconnectedcomponent
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-type Graph struct {
-	matrixLink map[string]List
+type GraphFindSCC struct {
+	matrixLink        map[string]List
+	markRecursCounter int
 }
 
-func (g *Graph) Print(nameVertex string) {
+type action func(string)
+
+func (g *GraphFindSCC) dfs(nameVertex string, funcAction action) {
 	vertexVisit := make(map[string]string, 0)
 	stack := NewList()
 	stack.PushFront(nameVertex)
@@ -18,7 +24,7 @@ func (g *Graph) Print(nameVertex string) {
 			continue
 		}
 		vertexVisit[vertex] = vertex
-		fmt.Println(vertex)
+		funcAction(vertex)
 		listAdjVertex := g.matrixLink[vertex]
 		if listAdjVertex == nil {
 			continue
@@ -29,4 +35,115 @@ func (g *Graph) Print(nameVertex string) {
 			node = node.Next
 		}
 	}
+}
+
+func (g *GraphFindSCC) Print(nameBeginVertex string) {
+	g.dfs(nameBeginVertex, func(vertex string) { fmt.Println(vertex) })
+}
+
+func isContains(list List, value string) bool {
+	if list == nil || list.Len() == 0 {
+		return false
+	}
+	node := list.Front()
+	for node != nil {
+		if node.Value.(string) == value {
+			return true
+		}
+		node = node.Next
+	}
+	return false
+}
+
+func (g *GraphFindSCC) invert() {
+	vertexVisit := make(map[string]string, 0)
+	for key, _ := range g.matrixLink {
+		_, ok := vertexVisit[key]
+		if ok {
+			continue
+		}
+		vertexVisit[key] = key
+		node := g.matrixLink[key].Front()
+		listVertexForRemove := make([]*ListItem, 0)
+		for node != nil {
+			vertex := node.Value.(string)
+			if !isContains(g.matrixLink[vertex], key) {
+				g.matrixLink[vertex].PushBack(key)
+				listVertexForRemove = append(listVertexForRemove, node)
+			}
+			node = node.Next
+		}
+		for _, v := range listVertexForRemove {
+			g.matrixLink[key].Remove(v)
+		}
+	}
+
+}
+func (g *GraphFindSCC) markLocal(markVertex map[string]int, vertexName string) {
+	if len(markVertex) == len(g.matrixLink) {
+		return
+	}
+	g.markRecursCounter++
+	markVertex[vertexName] = g.markRecursCounter
+	node := g.matrixLink[vertexName].Front()
+	for node != nil {
+		vertex := node.Value.(string)
+		_, ok := markVertex[vertex]
+		if ok {
+			node = node.Next
+			continue
+		}
+		g.markLocal(markVertex, vertex)
+		g.markRecursCounter++
+		markVertex[vertexName] = g.markRecursCounter
+		node = node.Next
+	}
+
+}
+func (g *GraphFindSCC) mark() []string {
+	g.markRecursCounter = 0
+	markVertex := make(map[string]int, 0)
+	//---mark---
+	for key := range g.matrixLink {
+		g.markLocal(markVertex, key)
+	}
+	//---sort---
+	sortVertex := make([]string, 0, len(markVertex))
+	for key := range markVertex {
+		sortVertex = append(sortVertex, key)
+	}
+	sort.SliceStable(sortVertex, func(i, j int) bool {
+		return markVertex[sortVertex[i]] > markVertex[sortVertex[j]]
+	})
+	return sortVertex
+}
+
+func (g *GraphFindSCC) find(sortVertex []string) map[int]List {
+	components := make(map[int]List, 0)
+	vertexVisit := make(map[string]string, 0)
+	numComponent := 0
+	for _, vertex := range sortVertex {
+		_, ok := vertexVisit[vertex]
+		if ok {
+			continue
+		}
+		vertexVisit[vertex] = vertex
+
+		g.dfs(vertex, func(key string) {
+			if components[numComponent] == nil {
+				components[numComponent] = NewList()
+			}
+			components[numComponent].PushBack(key)
+			vertexVisit[key] = key
+		})
+		numComponent++
+	}
+	return components
+}
+
+func (g *GraphFindSCC) FindStronglyConnectedComponents() map[int]List {
+	sortVertex := g.mark()
+	g.invert()
+	components := g.find(sortVertex)
+	return components
 }
--- 14-graph-strongly-connected-component/graph_strongly_connected_component_test.go
+++ 14-graph-strongly-connected-component/graph_strongly_connected_component_test.go
@@ -1,31 +1,79 @@
 package p14graphstronglyconnectedcomponent
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestGraphPrint(t *testing.T) {
-	var g Graph
-	g.matrixLink = make(map[string]List, 0)
-	g.matrixLink["A"] = NewList()
-	g.matrixLink["A"].PushBack("B")
-	g.matrixLink["A"].PushBack("E")
-	g.matrixLink["B"] = NewList()
-	g.matrixLink["B"].PushBack("A")
-	g.matrixLink["B"].PushBack("C")
-	g.matrixLink["C"] = NewList()
-	g.matrixLink["C"].PushBack("D")
-	g.matrixLink["D"] = NewList()
-	g.matrixLink["D"].PushBack("C")
-	g.matrixLink["D"].PushBack("F")
-	g.matrixLink["E"] = NewList()
-	g.matrixLink["E"].PushBack("F")
+func listVertexName(list List) []string {
+	result := make([]string, 0)
+	if list == nil || list.Len() == 0 {
+		return result
+	}
+	node := list.Front()
+	for node != nil {
+		result = append(result, node.Value.(string))
+		node = node.Next
+	}
+	return result
+}
 
-	g.Print("A")
+func sameStringSlice(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	// create a map of string -> int
+	diff := make(map[string]int, len(x))
+	for _, _x := range x {
+		// 0 value for int is 0, so just increment a counter for the string
+		diff[_x]++
+	}
+	for _, _y := range y {
+		// If the string _y is not in diff bail out early
+		if _, ok := diff[_y]; !ok {
+			return false
+		}
+		diff[_y] -= 1
+		if diff[_y] == 0 {
+			delete(diff, _y)
+		}
+	}
+	return len(diff) == 0
+}
 
-	/*require.Equal(t, true, trie.Search("apple"))
-	require.Equal(t, false, trie.Search("app"))
-	require.Equal(t, true, trie.StartsWith("app"))
-	trie.Insert("app")
-	require.Equal(t, true, trie.Search("app"))*/
+func TestGraphComponents(t *testing.T) {
+
+	graph := make(map[string]List, 0)
+	graph["A"] = NewList()
+	graph["A"].PushBack("B")
+	graph["B"] = NewList()
+	graph["B"].PushBack("A")
+	graph["B"].PushBack("C")
+	graph["C"] = NewList()
+	graph["C"].PushBack("D")
+	graph["D"] = NewList()
+	graph["D"].PushBack("C")
+
+	gfscc := GraphFindSCC{matrixLink: graph}
+	fmt.Println("reachability from A")
+	gfscc.Print("A")
+	fmt.Println("reachability from D")
+	gfscc.Print("D")
+
+	components := gfscc.FindStronglyConnectedComponents()
+	comp1 := listVertexName(components[0])
+	comp2 := listVertexName(components[1])
+	comp3 := listVertexName(components[2])
+	fmt.Println(comp1)
+	fmt.Println(comp2)
+	fmt.Println(comp3)
+	require.Equal(t, true, sameStringSlice([]string{"A", "B"}, comp1))
+	require.Equal(t, true, sameStringSlice([]string{"C", "D"}, comp2))
+
+	fmt.Println("reachability from A")
+	gfscc.Print("A")
+	fmt.Println("reachability from D")
+	gfscc.Print("D")
 }
