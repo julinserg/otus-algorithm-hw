7c8e7e12a6d61fa1fd2f597e6ff1de177926aaf8
ss.yulin <ss.yulin@gitlab.com>
2023-04-21 10:15:12 +0300

    fix test

--- 15-graph-topological-sort/graph_topological_sort_test.go
+++ 15-graph-topological-sort/graph_topological_sort_test.go
@@ -7,29 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func sameStringSlice(x, y []string) bool {
-	if len(x) != len(y) {
-		return false
-	}
-	// create a map of string -> int
-	diff := make(map[string]int, len(x))
-	for _, _x := range x {
-		// 0 value for int is 0, so just increment a counter for the string
-		diff[_x]++
-	}
-	for _, _y := range y {
-		// If the string _y is not in diff bail out early
-		if _, ok := diff[_y]; !ok {
-			return false
-		}
-		diff[_y] -= 1
-		if diff[_y] == 0 {
-			delete(diff, _y)
-		}
-	}
-	return len(diff) == 0
-}
-
 func TestGraphSort(t *testing.T) {
 	graph := make([]NodeGraph, 0)
 	graph = append(graph, NodeGraph{"A", []string{"B"}})
@@ -45,6 +22,6 @@ func TestGraphSort(t *testing.T) {
 
 	result := gfscc.Sort()
 	fmt.Println(result)
-	require.Equal(t, true, sameStringSlice([]string{"C", "D", "A", "F", "B", "E", "G", "H"}, result))
+	require.Equal(t, []string{"C", "D", "A", "F", "B", "E", "G", "H"}, result)
 
 }
