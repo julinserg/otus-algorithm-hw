ae9e7f2768c3afde83f15c14d8898d9a50761ecc
ss.yulin <ss.yulin@gitlab.com>
2023-06-21 13:20:14 +0300

    add O^3 solve

--- 23-hangar/hangar.go
+++ 23-hangar/hangar.go
@@ -1,18 +1,5 @@
 package p23hangar
 
-/*func findMaxHeight(i, j int) int {
-	maxHeight := 0
-	return maxHeight
-}
-
-func findMaxWidth(i, j int) int {
-	maxWidth := 0
-	for i := 0; i < count; i++ {
-
-	}
-	return maxWidth
-}*/
-
 func findLocalMaxSquare(garden [][]int, i, j int) int {
 	maxSquare := 0
 	h := 0
@@ -34,6 +21,63 @@ func findLocalMaxSquare(garden [][]int, i, j int) int {
 	return maxSquare
 }
 
+func findLocalMaxSquareWithOptimization(garden [][]int, i, j int) int {
+	maxSquare := 0
+	h := 0
+	wLimit := j + 1
+	for i-h >= 0 && h <= i+1 && garden[i-h][j] != 1 {
+		w := 0
+		for w < wLimit && j-w >= 0 && w <= j+1 && garden[i-h][j-w] != 1 {
+			w++
+		}
+		if w < wLimit {
+			wLimit = w
+		}
+		if wLimit*(i+1) < maxSquare {
+			break
+		}
+		h++
+		square := wLimit * h
+		if square > maxSquare {
+			maxSquare = square
+		}
+	}
+	return maxSquare
+}
+
+func calcColumnWidths(j int, garden [][]int, widths []int) []int {
+	N := len(garden)
+	for i := 0; i < N; i++ {
+		if garden[i][j] == 1 {
+			widths[i] = 0
+		} else {
+			widths[i]++
+		}
+	}
+	return widths
+}
+
+func findLocalMaxSquareO3(garden [][]int, i, j int, widths []int) int {
+	maxSquare := 0
+	h := 0
+	wLimit := j + 1
+	for i-h >= 0 && h <= i+1 && garden[i-h][j] != 1 {
+		w := widths[i-h]
+		if w < wLimit {
+			wLimit = w
+		}
+		if wLimit*(i+1) < maxSquare {
+			break
+		}
+		h++
+		square := wLimit * h
+		if square > maxSquare {
+			maxSquare = square
+		}
+	}
+	return maxSquare
+}
+
 func hangarBruteForce(garden [][]int) int {
 	N := len(garden)
 	M := len(garden[0])
@@ -41,6 +85,38 @@ func hangarBruteForce(garden [][]int) int {
 	for i := 0; i < N; i++ {
 		for j := 0; j < M; j++ {
 			square := findLocalMaxSquare(garden, i, j)
+			if square > maxSquare {
+				maxSquare = square
+			}
+		}
+	}
+	return maxSquare
+}
+
+func hangarBruteForceWithOptimization(garden [][]int) int {
+	N := len(garden)
+	M := len(garden[0])
+	maxSquare := 0
+	for i := 0; i < N; i++ {
+		for j := 0; j < M; j++ {
+			square := findLocalMaxSquareWithOptimization(garden, i, j)
+			if square > maxSquare {
+				maxSquare = square
+			}
+		}
+	}
+	return maxSquare
+}
+
+func hangarO3(garden [][]int) int {
+	N := len(garden)
+	M := len(garden[0])
+	maxSquare := 0
+	widths := make([]int, N)
+	for j := 0; j < M; j++ {
+		widths = calcColumnWidths(j, garden, widths)
+		for i := 0; i < N; i++ {
+			square := findLocalMaxSquareO3(garden, i, j, widths)
 			if square > maxSquare {
 				maxSquare = square
 			}
--- 23-hangar/hangar_test.go
+++ 23-hangar/hangar_test.go
@@ -37,3 +37,67 @@ func TestHangarBruteForce(t *testing.T) {
 	square2 := hangarBruteForce(garden2)
 	require.Equal(t, 8, square2)
 }
+
+func TestHangarBruteForceWithOptimization(t *testing.T) {
+	garden := [][]int{
+		{0, 0, 0, 0, 0, 0},
+		{0, 1, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+	}
+	square := hangarBruteForceWithOptimization(garden)
+	require.Equal(t, 12, square)
+
+	garden1 := [][]int{
+		{0, 0, 1, 0, 0, 0},
+		{0, 1, 0, 1, 0, 0},
+		{1, 1, 0, 0, 1, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+	}
+	square1 := hangarBruteForceWithOptimization(garden1)
+	require.Equal(t, 8, square1)
+
+	garden2 := [][]int{
+		{0, 0, 0, 1, 0, 0},
+		{0, 0, 1, 1, 0, 0},
+		{1, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+	}
+	square2 := hangarBruteForceWithOptimization(garden2)
+	require.Equal(t, 8, square2)
+}
+
+func TestHangarO3(t *testing.T) {
+	garden := [][]int{
+		{0, 0, 0, 0, 0, 0},
+		{0, 1, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+	}
+	square := hangarO3(garden)
+	require.Equal(t, 12, square)
+
+	garden1 := [][]int{
+		{0, 0, 1, 0, 0, 0},
+		{0, 1, 0, 1, 0, 0},
+		{1, 1, 0, 0, 1, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+	}
+	square1 := hangarO3(garden1)
+	require.Equal(t, 8, square1)
+
+	garden2 := [][]int{
+		{0, 0, 0, 1, 0, 0},
+		{0, 0, 1, 1, 0, 0},
+		{1, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0},
+	}
+	square2 := hangarO3(garden2)
+	require.Equal(t, 8, square2)
+}
