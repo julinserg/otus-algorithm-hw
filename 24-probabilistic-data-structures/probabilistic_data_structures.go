8473182a8c9d32301966ce6cfb45501f5cccb0f6
ss.yulin <ss.yulin@gitlab.com>
2023-06-23 18:55:43 +0300

    hyperloglog remove

--- 24-probabilistic-data-structures/probabilistic_data_structures.go
+++ 24-probabilistic-data-structures/probabilistic_data_structures.go
@@ -1,18 +1,8 @@
 package p24probabilisticdatastructures
 
 import (
-	"hash/crc32"
-
 	"github.com/dustin/go-probably"
 )
-
-func countUniqueHyperLogLog(data []string) uint64 {
-	hll := probably.NewHyperLogLog(0.0001)
-	for _, d := range data {
-		hll.Add(crc32.ChecksumIEEE([]byte(d)))
-	}
-	return hll.Count()
-}
 
 func countUniqueCountMinSketch(data []string, query string) uint64 {
 	sk := probably.NewSketch(1<<20, 5)
