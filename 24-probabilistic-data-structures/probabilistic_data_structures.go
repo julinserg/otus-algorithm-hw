e93fe697bdcf4185e7a88abae9a031d77bdf8064
ss.yulin <ss.yulin@gitlab.com>
2023-06-23 19:29:14 +0300

    dataset from Kaggle

--- 24-probabilistic-data-structures/probabilistic_data_structures.go
+++ 24-probabilistic-data-structures/probabilistic_data_structures.go
@@ -1,13 +1,1 @@
 package p24probabilisticdatastructures
-
-import (
-	"github.com/dustin/go-probably"
-)
-
-func countUniqueCountMinSketch(data []string, query string) uint64 {
-	sk := probably.NewSketch(1<<20, 5)
-	for _, d := range data {
-		sk.Increment(d)
-	}
-	return uint64(sk.Count(query))
-}
--- 24-probabilistic-data-structures/probabilistic_data_structures_test.go
+++ 24-probabilistic-data-structures/probabilistic_data_structures_test.go
@@ -1,7 +1,14 @@
 package p24probabilisticdatastructures
 
 import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
 	"testing"
+
+	"github.com/dustin/go-probably"
+	"github.com/stretchr/testify/require"
 )
 
 type CountMinSketchTestData struct {
@@ -51,6 +58,53 @@ func runCountMinSketchTests(t *testing.T, f func(data []string, query string) ui
 	}
 }
 
+func countUniqueCountMinSketch(data []string, query string) uint64 {
+	sk := probably.NewSketch(1<<20, 5)
+	for _, d := range data {
+		sk.Increment(d)
+	}
+	return uint64(sk.Count(query))
+}
+
 func TestCountMinSketch(t *testing.T) {
 	runCountMinSketchTests(t, countUniqueCountMinSketch, "countUniqueCountMinSketch", countMinSketchTestData)
+}
+
+func TestCountMinSketchOnBigData(t *testing.T) {
+	// https://www.kaggle.com/datasets/datasnaek/youtube-new
+	file, err := os.Open("RUvideos.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	sk := probably.NewSketch(1<<20, 5)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineParse := strings.Split(line, ",")
+		tagsLine := lineParse[6]
+		tagsLine = strings.ReplaceAll(tagsLine, "\"", "")
+		tags := strings.Split(tagsLine, "|")
+		for _, tag := range tags {
+			sk.Increment(tag)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	require.Equal(t, 367, int(sk.Count("футбол")))
+	require.Equal(t, 84, int(sk.Count("хоккей")))
+	require.Equal(t, 43, int(sk.Count("бокс")))
+	require.Equal(t, 33, int(sk.Count("смешанные единоборства")))
+	require.Equal(t, 32, int(sk.Count("плавание")))
+	require.Equal(t, 12, int(sk.Count("бег")))
+	require.Equal(t, 13, int(sk.Count("баскетбол")))
+	require.Equal(t, 5, int(sk.Count("теннис")))
+	require.Equal(t, 1, int(sk.Count("волейбол")))
+	require.Equal(t, 0, int(sk.Count("триатлон")))
+	require.Equal(t, 0, int(sk.Count("трейлранинг")))
+	require.Equal(t, 1379, int(sk.Count("политика")))
 }
